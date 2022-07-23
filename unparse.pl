8e075aa836173fd1e568bae9741fb236e5cc37a4
Tatsuo Mitsuchi <t.mitsuchi3@gmail.com>
2022-07-24 01:19:17 +0900

    ok

--- unparse.pl
+++ unparse.pl
@@ -1,12 +1,12 @@
-[50, ++, 51] >> a(++).
-[60, **, 61] >> a(**).
-[if, 30, then, 30, else, 31] << a(**).
+[50, ++, 51] >> a(++, 50).
+[60, **, 61] >> a(**, 60).
+[if, 30, then, 30, else, 31] << a(if, 90).
 
 str(A, A) :- atom(A); number(A).
 str(E, Str) :-
     functor(E, _, _, compound),
     E =.. [Op | Terms],
-    As >> a(Op),
+    (As >> a(Op, _); As << a(Op, _)),
     str_each(Op, As, Terms, Strs),
     atomic_list_concat(Strs, ' ', Str).
 
@@ -23,7 +23,7 @@ str(A, _, A) :- atom(A); number(A).
 str(E, Op1, Str) :-
     functor(E, _, _, compound),
     E =.. [Op2 | _],
-    [A2|_] >> a(Op2),
-    [A1|_] >> a(Op1),
-    (A1 > A2 -> str(E, Str1), atomic_list_concat(['(', Str1, ')'], ' ', Str)
+    (_ >> a(Op2, P2); _ << a(Op2, P2)),
+    (_ >> a(Op1, P1); _ << a(Op1, P1)),
+    (P1 > P2 -> str(E, Str1), atomic_list_concat(['(', Str1, ')'], '', Str)
             ; str(E, Str)).
