fb8cf08215612884b929533c1de17e500b0972d0
Tatsuo Mitsuchi <t.mitsuchi3@gmail.com>
2022-07-30 21:17:01 +0900

    いったん

--- ron.pl
+++ ron.pl
@@ -335,11 +335,45 @@ test_arrow4 :- code_mi("op 50 : _ -> _ ; 1 -> 2; 2 -> 3; main { 2 -> 3; }").
 test_arrow5 :- code_mi("op 50 : _ -> _ ; op 50 : _ => _ ; 1 -> 2; 2 -> 3; x => y {x -> y; } x => y {x -> z; z => y; } main { 1 => 2; }").
 test_type1 :- code_mi("op 50 : _ |- _ : _ ; _ |- true : bool; main { c |- true : x; }").
 test_succ :- code_mi("op 50 : _ + _ = _ ; op 50 : succ _ ; 0 + n = n; succ m + n = succ p {m + n = p; } main {succ 0 + succ 0 = x; }").
-test_type2 :- code_mi("op 50 : if _ then _ else _ ; op 50 : _ |- _ : _ ; _ |- 0 : int; c |- if e1 then e2 else e3 : t { c |- e1 : bool; c |- e2 : t; c |- e3 : t; } main { _ |- if true then 0 else 0 : t ; }").
+test_type2 :- code_mi("op 50 : if _ then _ else _ ; op 50 : _ |- _ : _ ; _ |- 0 : int; _ |- true : bool; c |- if e1 then e2 else e3 : t { c |- e1 : bool; c |- e2 : t; c |- e3 : t; } main { _ |- if true then 0 else 0 : t ; }").
 test_plus :- code_mi("op 50 : _ + _ = _ ; op 50 : succ _ ; op 50 : _ plus _ is _ ; 0 plus n is n; (succ m) plus n is (succ p) { m plus n is p; } main { (succ 0) plus (succ 0) is x ; }").
-test_eval_if :- code_mi("op 50 : _ => _ ; op 50 : if _ then _ else _ ; op 50 : _ |- _ : _ ; c |- 0 => 0; c |- 1 => 1; c |- true => true; c |- if e1 then e2 else e3 => v { c |- e1 => true; c |- e2 => v; } main { _ |- if true then 0 else 1 => v ; }").
+test_eval_if :- code_mi("op 50 : _ |- _ => _ ; op 50 : if _ then _ else _ ; op 50 : _ |- _ : _ ; c |- 0 => 0; c |- 1 => 1; c |- true => true; c |- if e1 then e2 else e3 => v { c |- e1 => true; c |- e2 => v; } main { _ |- if true then 0 else 1 => v ; }").
 
 test_skip :- code_mi("; op 50 : _ -> _ ; 1 -> 2; main { 1 -> 2; } ; ;").
+
+test_eval_if_lf :- code_mi("
+    op 50 : _ |- _ => _
+    op 50 : if _ then _ else _
+    
+    c |- 0 => 0
+    c |- 1 => 1
+    c |- true => true
+    c |- if e1 then e2 else e3 => v {
+        c |- e1 => true
+        c |- e2 => v
+    }
+    
+    main {
+        _ |- if true then 0 else 1 => v
+    }
+    ").
+
+test_type2_lf :- code_mi("
+    op 50 : if _ then _ else _
+    op 50 : _ |- _ : _ 
+
+    _ |- 0 : int
+    _ |- true : bool
+    c |- if e1 then e2 else e3 : t {
+        c |- e1 : bool
+        c |- e2 : t
+        c |- e3 : t
+    }
+
+    main {
+        _ |- if true then 0 else 0 : t
+    }
+    ").
 
 test_arrow1_lf :- code_mi("
     op 50 : _ -> _
@@ -360,7 +394,8 @@ test_lf :-
         2 -> 3
         main {
             x -> 3
-        }").
+        }
+        ").
 
 test_edge :-
     code_mi("op 50 : _ -> _ ;"),
@@ -372,8 +407,17 @@ test_app :-
     code_mi("op 100 : _ _ ;"),
     code_pred("1 + 2 + 3", W), writeln(W).
 
-test_ski_lf :-
-    code_mi("op 50 : _ -> _ ; op 50 : _ => _ ; op 100 : _ _ ;I x -> x ;").
+test_ski_lf :- code_mi("
+    op 50 : _ -> _
+    op 50 : _ => _ 
+    op 100 : _ _
+
+    I x -> x
+    K x y -> x
+    S x y z -> x z (y z)
+    
+    1 -> 2
+    ").
 
 test_ski :-
     code_mi("op 50 : _ -> _ ;"),
