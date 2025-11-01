787ab3c0d754efdec128ba510d8d7f0e71deeca9
Tatsuo Mitsuchi <t.mitsuchi3@gmail.com>
2025-11-01 12:50:26 +0900

    tapl record pattern

--- test.sh
+++ test.sh
@@ -127,6 +127,7 @@ test_all() {
     run_test "example/tapl.pair.ron" "v = succ 0"
     run_test "example/tapl.tuple.ron" "v = succ 0"
     run_test "example/tapl.record.ron" "T = Nat"
+    run_test "example/tapl.record.pattern.ron" "v = { succ 0 , true }"
 
     echo "========================================"
     echo "Results: $PASS passed, $FAIL failed"
